64f1da2374a23e2539702b61e6dc23f921bbeb4a
Greg Wiley <aztec.rex@jammm.com>
2018-01-28 21:32:07 -0800

    improve builder

--- builder/build.sh
+++ builder/build.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env bash
 
 cd /build
-stack exec test-exe
+stack build
+stack install
 
+lib='/build/.stack-work/install/x86_64-linux/lts-10.4/8.2.2/lib/libhelloFromHaskell.so'
+dest='/output/lib'
 
+rm -rf /output/*
+mkdir -p "$dest"
+cp "$lib" "/output/$(basename $lib)"
+tmpfile=$(mktemp)
+ldd "$lib" | cut -d' ' -f 3 > $tmpfile
+for i in $(cat $tmpfile); do
+   cp "$i" "${dest}/$(basename $i)"
+done
+rm $tmpfile
