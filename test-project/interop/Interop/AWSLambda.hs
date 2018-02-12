71aa899aa1c38dc4fe856f0270a999b08039d527
Greg Wiley <gwiley@cj.com>
2018-02-11 19:53:49 -0800

    toward a lambda fn monad

--- test-project/interop/Interop/AWSLambda.hs
+++ test-project/interop/Interop/AWSLambda.hs
@@ -1,11 +1,18 @@
-module Interop.AWSLambda(makeLambda) where
+module Interop.AWSLambda(makeLambda, runLambdaT, event) where
 
+import Control.Monad.Trans.Class (MonadTrans, lift)
 import Foreign.C (CString, newCString)
 import Foreign.Marshal.Alloc(free)
-import Data.Aeson (encode, decodeStrict, FromJSON, ToJSON)
+import Data.Aeson (encode, decodeStrict, FromJSON, ToJSON, Object)
+import Data.Aeson.Types (emptyObject)
 import Data.ByteString (packCString, useAsCString, ByteString)
-import Data.ByteString.Lazy(toStrict)
+import Data.ByteString.Lazy (toStrict)
+import Data.ByteString.Builder (toLazyByteString)
 import Data.IORef (IORef, newIORef, atomicModifyIORef')
+import Control.Applicative (Applicative, liftA2)
+
+defaultObject :: ByteString
+defaultObject = encodeStrict emptyObject
 
 {-
 This just holds a reference to the last-returned value. Otherwise it
@@ -43,3 +50,33 @@ makeLambda f event = do
           b <- iob
           unpackCString $ encodeStrict b
         _ -> clear
+
+
+newtype LambdaT p m a = LambdaT { runLambdaT :: p -> m a }
+
+instance (Functor m) => Functor (LambdaT p m) where
+    fmap f (LambdaT fa) = LambdaT $ (fmap .fmap) f fa
+
+instance (Applicative m) => Applicative (LambdaT p m) where
+    pure = liftLambdaT . pure
+    LambdaT mf <*> LambdaT mx = LambdaT $ liftA2 (<*>) mf mx
+
+chl :: (Monad m) => LambdaT p m a -> (a -> LambdaT p m b) -> LambdaT p m b
+chl (LambdaT rf) f = LambdaT $ \p -> do
+    a <- rf p
+    runLambdaT (f a) p
+
+instance (Monad m) => Monad (LambdaT p m) where
+    ma >>= f = chl ma f
+
+
+liftLambdaT :: (Functor m) => m a -> LambdaT p m a
+liftLambdaT mx = LambdaT $ const mx
+
+instance MonadTrans (LambdaT p) where
+    lift = liftLambdaT
+
+event :: (Monad m) => LambdaT p m p
+event = LambdaT pure
+
+
