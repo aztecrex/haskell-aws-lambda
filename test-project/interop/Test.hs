f14c15471a1cf0ae0a6fd1f902727594ac44eaa9
Greg Wiley <gwiley@cj.com>
2018-02-19 09:42:06 -0800

    with sementic data

--- test-project/interop/Test.hs
+++ test-project/interop/Test.hs
@@ -1,26 +1,49 @@
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE OverloadedStrings #-}
 module Test where
 
 import Foreign.C (CString, newCString)
 import Data.Aeson.Types (Object, emptyObject)
+import Data.Aeson (ToJSON, FromJSON)
 import Data.HashMap.Strict (insert)
+import GHC.Generics
+import Data.Text
 
 import Cloud.Compute.AWS.Lambda (interop, toSerial, LambdaT, argument, runLambdaT)
 
--- transform :: (Monad m) => Object -> m (Either Object Object)
--- transform = pure . Right
 
+data MathProblem = MathAdd {x :: Int, y :: Int } | MathMultiply { x :: Int, y :: Int} | MathZoo { animal :: Text }
+    deriving (Show, Generic)
+data MathAnswer = MathAnswer { answer :: Int }
+    deriving (Show, Generic)
+data MathError = MathError { description :: Text }
+    deriving (Show, Generic)
 
-barm :: LambdaT Object Object IO Object
+instance ToJSON MathAnswer where
+instance ToJSON MathError where
+instance FromJSON MathProblem where
+instance ToJSON MathProblem where
+
+data ParseError = ParseError
+    deriving (Show, Generic)
+
+instance ToJSON ParseError
+
+barm :: LambdaT Object Object IO MathProblem
 barm = do
-    input <- argument
-    pure $ insert "added" "yo yo yo" input
+    pure $ MathAdd 101 993
+    -- problem <- argument
+    -- case problem of
+    --     Add x y -> pure $ MathAnswer (x + y)
+    --     Multiply x y -> pure $ MathAnswer (x * y)
+    --     MathZoo _ -> pure $ MathError "there is no math at the zoo"
+    -- pure $ insert "added" "yo yo yo" input
 
 
-barf :: Object -> IO (Either Object Object)
+barf :: Object -> IO (Either Object MathProblem)
 barf = runLambdaT barm
 
 foreign export ccall bar :: CString -> IO CString
 bar :: CString -> IO CString
-bar = interop $ toSerial emptyObject barf
+bar = interop $ toSerial ParseError barf
 
